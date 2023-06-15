6877cf94127537861d07928358399dcb539ad029
Cheema Mahesh <maheshbabucheema789@gmail.com>
2023-06-16 01:06:02 +0530

    update with the lastes changes

--- routs/users.js
+++ routs/users.js
@@ -6,6 +6,8 @@ const router=express.Router();
 const usersController=require('../controllers/user_controller');
 
 router.get('/profile',usersController.profile);
+router.get('/sign-up',usersController.signup,);
+router.get('/sign-in',usersController.signin);
 
 
 
