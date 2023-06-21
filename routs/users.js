464a42f58392f87d27368ecc8ade5d81a1120be7
Cheema Mahesh <maheshbabucheema789@gmail.com>
2023-06-21 19:48:01 +0530

    update with latest changes

--- routs/users.js
+++ routs/users.js
@@ -6,7 +6,7 @@ const passport=require('passport');
 
 const usersController=require('../controllers/user_controller');
 
-router.get('/profile',usersController.profile);
+router.get('/profile',passport.checkAuthentication,usersController.profile);
 router.get('/sign-up',usersController.signup,);
 router.get('/sign-in',usersController.signin);
 
@@ -17,9 +17,12 @@ router.post('/create',usersController.create);
 
 
 //use passport as middleware to authenticate
-router.post('/create-session',passport.authenticate('local',{
-    failureRedirect:'/users/sign-in'
-}),usersController.createSession)
+router.post('/create-session',
+
+passport.authenticate('local',{
+    failureRedirect:'/users/sign-in'})
+    
+,usersController.createSession)
 
 
 module.exports=router;
