4159f4a0ae70ef4b377a94a1cd2ced0d9e342160
Cheema Mahesh <maheshbabucheema789@gmail.com>
2023-06-24 17:57:50 +0530

    async Update

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,42 +1,22 @@
 const Post=require('../models/post');
 const Comment=require('../models/comment');
 
-module.exports.create=function(req,res){
-    Post.create({
-        content:req.body.content,
-        user:req.user._id
-    }).then((post)=>{
-        return res.redirect('back')
-    }).catch((err)=>{
-        if(err){console.log("error in creating a post"); return}
-    })
+module.exports.create= async function(req,res){
+
+  try{
+    let post=await Post.create({
+      content:req.body.content,
+      user:req.user._id
+  })
+  return res.redirect('back');
+
+  }catch(err){
+    console.log("error in creating a post",err); 
+    return;
+
+  }
 }
 
-
-// module.exports.destroy=function(req,res){
-//     Post.findById(req.params.id).then((post)=>{
-
-//         //.id means converting the object id into string
-//         if(post.user==req.user.id){
-
-//             post.remove();
-//             Comment.deleteMany({post:req.params.id}).then((mahi)=>{
-//                 return res.redirect('back');
-//             });
-
-//         }else{
-//             return res.redirect('back');
-//         }
-
-
-//     }).catch((err)=>{
-
-//         if(err){
-//             console.log("error in deleting a post",err);
-//         }
-
-//     })
-// }
 
 
 
@@ -62,6 +42,7 @@ module.exports.destroy = async function (req, res) {
       }
     } catch (err) {
       console.log("Error in deleting a post:", err);
+      return;
       // Handle the error response appropriately
     }
   };
